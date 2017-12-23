bed1e93fe250611f41fa402db6f1cef8c051094f
dieg0 <dieg0card0s0@hotmail.com>
2017-12-23 14:26:05 -0200

    add git page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game-of-life
+React: https://diegocardoso93.github.io/game-of-life/react/build/
--- react/src/GameHeader.jsx
+++ react/src/GameHeader.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 
 const GameHeader = () =>
   <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src="favicon.ico" className="App-logo" alt="logo" />
     <h1 className="App-title">Game of Life</h1>
   </header>;
 
